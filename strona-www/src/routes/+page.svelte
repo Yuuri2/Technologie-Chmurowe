04f1e9adacbe4591df626f725552c1e1b2aeb70c
Adrian Torbus <adrtor222@gmail.com>
2026-03-24 20:57:54 +0100

    Dodano stronę glowna

--- strona-www/src/routes/+page.svelte
+++ strona-www/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section id="main">
+<h1>Lista Zakupów</h1>
+<h2>Funkcjonalności do dodania</h2>
+<p>logowanie przy użyciu bazy</p>
+<p>Strona główna "menu"</p>
+<p>Logika listy zakupów z opcjami</p>
+</section>
+
+<style>
+html{
+    background-color: skyblue;
+}
+
+</style>

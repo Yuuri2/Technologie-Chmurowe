2361133c15c69d1fbd6e1681b0758a3526043bcf
Adrian Torbus <adrtor222@gmail.com>
2026-06-04 18:31:28 +0200

    changed db.ts file struct

--- strona-www/src/lib/server/db.ts
+++ strona-www/src/lib/server/db.ts
@@ -1,5 +1,15 @@
 import pg from 'pg';
+import { env } from '$env/dynamic/private';
 
 const { Pool } = pg;
 
-export const db = new Pool({});
+// Sprawdzamy czy zmienna istnieje, jeśli nie (np. lokalnie), dajemy czytelny fallback
+const connectionString = env.DATABASE_URL;
+
+if (!connectionString) {
+    console.warn("⚠️ Ostrzeżenie: Brak zmiennej DATABASE_URL w środowisku!");
+}
+
+export const db = new Pool({
+    connectionString: connectionString
+});

fb9b0e96e4f6e29d1fed24627af3cd55326868b9
Adrian Torbus <adrtor222@gmail.com>
2026-06-04 17:19:49 +0200

    Added e2e tests using Playwright

---
+++ strona-www/playwright.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 1, // 1 worker, aby uniknąć konfliktów jednoczesnych żądań do bazy
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  // Ta sekcja automatycznie zbuduje i odpali serwer SvelteKita przed uruchomieniem testów
+  webServer: {
+    command: 'npm run build && npm run preview',
+    port: 4173,
+    reuseExistingServer: !process.env.CI,
+  },
+});

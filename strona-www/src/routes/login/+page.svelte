83c03f11b4da2ae82bbe604bccb73e06a3426bcd
asag22 <letypdiwad@gmail.com>
2026-06-04 23:37:25 +0200

    nie bo tym razem zadziała

--- strona-www/src/routes/login/+page.svelte
+++ strona-www/src/routes/login/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
-    alert("login")
+    console.log("login");
+
+    let login = ()=>{alert('login')}
 </script>
 <div>
     <h1>login</h1>
+    <button onclick={login}>
+        login
+    </button>
 </div>

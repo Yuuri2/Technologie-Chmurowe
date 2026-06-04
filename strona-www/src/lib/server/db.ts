2c239ca07959f0dc5bd559f4e35d19538a91a71d
asag22 <letypdiwad@gmail.com>
2026-06-04 19:21:50 +0200

    związane z poprzednim teraz jest enviorment varible inny

--- strona-www/src/lib/server/db.ts
+++ strona-www/src/lib/server/db.ts
@@ -1,15 +1,6 @@
 import pg from 'pg';
-import { env } from '$env/dynamic/private';
 
 const { Pool } = pg;
 
-// Sprawdzamy czy zmienna istnieje, jeśli nie (np. lokalnie), dajemy czytelny fallback
-const connectionString = env.DATABASE_URL;
 
-if (!connectionString) {
-    console.warn("⚠️ Ostrzeżenie: Brak zmiennej DATABASE_URL w środowisku!");
-}
-
-export const db = new Pool({
-    connectionString: connectionString
-});
+export const db = new Pool();

69ff65d61545a61df414ec32c38cb14467c83268
asag22 <letypdiwad@gmail.com>
2026-06-07 20:04:37 +0200

    teraz działa poprawnie form

--- strona-www/src/routes/home/+page.svelte
+++ strona-www/src/routes/home/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
     import LogoutButton from "$lib/components/LogoutButton.svelte";
-    let { data } = $props();
+    import { enhance } from "$app/forms";
+    let { data, form } = $props();
 </script>
 <div>
     <h1>home</h1><LogoutButton></LogoutButton>
-    <form action="/home/createList">
+    <form method="POST" action="/home/createList" use:enhance>
         <input type="text" name="nazwa" id="nazwa">
         <button type="submit">stwórz</button>
+        {#if form?.error}
+            <p style="color: red;" >{form.error}</p>
+        {/if}
     </form>
     {#each data.lists as list }
         <div>

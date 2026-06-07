c6eb36d4c4994549a5b9cda3b4f00180ee216fee
Adrian Torbus <adrtor222@gmail.com>
2026-06-07 17:31:41 +0200

    change

--- strona-www/src/routes/products/+page.server.ts
+++ strona-www/src/routes/products/+page.server.ts
@@ -1,9 +1,11 @@
 import type { PageServerLoad } from './$types';
 import { pool } from '$lib/server/database/pool';
 
-export const load: PageServerLoad = async () => {
+
+export const load: PageServerLoad = async ({ locals, params }) => {
+    const list = params.slug;
     try {
-        const productsResult = await pool.query('SELECT * FROM lists WHERE owner LIKE x AND list LIKE y');
+        const productsResult = await pool.query('SELECT * FROM lists WHERE owner LIKE $1 AND list LIKE $2',[locals.user , list]);
 
         return {
             dbProducts: productsResult.rows

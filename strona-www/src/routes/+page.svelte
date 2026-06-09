f440ea90fa05c5a5f266464f0316ba1f9547ee32
Dawid Pytel <letypdiwad@gmail.com>
2026-06-09 10:25:54 +0200

    test

--- strona-www/src/routes/+page.svelte
+++ strona-www/src/routes/+page.svelte
@@ -7,6 +7,7 @@
     <div id="front">
         <div id="frontPageSign">
             <h1>Shopping list Center v2.0</h1>
+            <h1>TEST</h1>
         </div>
         <div id="SignInBtnContainer">
             <button class="UIButton" onclick={() => goto('/login')}>Log In</button>
